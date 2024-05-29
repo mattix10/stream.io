96e66acf710a735604739e36bf29b1bd5abf4562
mattix10 <mateusz.salamon10@gmail.com>
2024-05-29 09:51:06 +0200

    remove environment url from upload

--- src/app/core/services/file-upload-service/file-upload.service.ts
+++ src/app/core/services/file-upload-service/file-upload.service.ts
@@ -1,6 +1,6 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environment/environment';
 
 @Injectable()
@@ -11,14 +11,10 @@ export class FileUploadService {
     const formData: FormData = new FormData();
     formData.append('file', file);
 
-    return this.#httpClient.post(
-      `${environment.API_URL}/${uploadLink}`,
-      formData,
-      {
-        reportProgress: true,
-        observe: 'events',
-      }
-    );
+    return this.#httpClient.post(`${uploadLink}`, formData, {
+      reportProgress: true,
+      observe: 'events',
+    });
   }
 
   getLinkForUploadMovie(): Observable<string> {
