13b90f84f9d7b51bbc0b95f70594018c54ceb8cd
mattix10 <mateusz.salamon10@gmail.com>
2024-08-03 13:31:27 +0200

    change nip and phone number type

--- src/app/features/auth/models/registration-content-creator-request.ts
+++ src/app/features/auth/models/registration-content-creator-request.ts
@@ -2,6 +2,6 @@ import { BaseRegistrationRequest } from './base-registration-request';
 
 export interface RegistrationContentCreatorRequest
   extends BaseRegistrationRequest {
-  nip: number;
-  phoneNumber: number;
+  nip: string;
+  phoneNumber: string;
 }
