6b5be247dca46164eae4f2a0f07d7c68ff454b28
mattix10 <mateusz.salamon10@gmail.com>
2024-08-20 23:51:38 +0200

    update user comments

---
+++ mocks/test-data/comments/user-comments.ts
@@ -0,0 +1,29 @@
+const comments = [
+  // Django
+  'Django to fascynujący film. Brutalność i humor sprawiają, że oglądanie było naprawdę wciągające.',
+  'Świetna gra aktorska i niesamowita fabuła. Tarantino znowu dostarczył film pełen napięcia.',
+
+  // Ojciec Chrzestny
+  'Ojciec Chrzestny to arcydzieło. Byłem pod wrażeniem złożoności postaci i mrocznej atmosfery.',
+  'Ten film zrobił na mnie ogromne wrażenie. Klasyczna saga mafijna, którą każdy powinien zobaczyć.',
+
+  // Mroczny Rycerz
+  'Świetny film o Batmanie. Joker w wykonaniu Ledgera był naprawdę przerażający i fascynujący.',
+  'Mroczny Rycerz trzyma w napięciu od początku do końca. Fantastyczne efekty specjalne i głęboka fabuła.',
+
+  // Fight Club
+  'Fight Club to niesamowity film z zaskakującym zakończeniem. Edward Norton i Brad Pitt byli świetni.',
+  'Film zmusił mnie do zastanowienia się nad współczesnym społeczeństwem. Zdecydowanie warty obejrzenia.',
+
+  // Matrix
+  'Matrix to film, który zmienił kino. Efekty specjalne i filozoficzne pytania były naprawdę imponujące.',
+  'Keanu Reeves jako Neo był świetny. Matrix to film pełen akcji i głębszego przesłania.',
+
+  // Władca Pierścieni: Powrót Króla
+  'Powrót Króla to epickie zakończenie trylogii. Wspaniała fabuła i niesamowite efekty specjalne.',
+  'Byłem pod wrażeniem skali i piękna tego filmu. Powrót Króla to prawdziwa uczta dla oczu.',
+
+  // Incepcja
+  'Incepcja to niesamowity film, który trzyma w napięciu do samego końca. Zawiła fabuła i świetne efekty specjalne.',
+  'Christopher Nolan znowu to zrobił! Film pełen akcji i zagadek, które sprawiają, że myślisz o nim długo po zakończeniu.',
+];
